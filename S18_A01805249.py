ddcf50a8a959a8f842876e792690aec8a9390093
alfonso12372864 <aabrajanvigil@outlook.com>
2025-10-08 17:56:36 -0600

    Update S18_A01805249.py

--- S18_A01805249.py
+++ S18_A01805249.py
@@ -42,4 +42,61 @@ for c in califs:
 porcentaje = (aprobados / len(califs)) * 100
 print("Porcentaje de aprobados:", porcentaje, "%")
 
+#ejercicio 3
+
+
+listaCompras = ["gatorade","cervezas","carne"]
+estadoCompras = [False,False,False]
+
+for i in range(len(listaCompras)):
+    if estadoCompras[i] == False:
+        respuesta = input("¿Ya compraste " + listaCompras[i] + "? (si/no): ")
+        if respuesta == "si":
+            estadoCompras[i] = True
+
+print("Estado de las compras:", estadoCompras)
+
+
+
+#ejercicio4
+
+
+numerosRandom = [8,2,15,4,7,1,10]
+
+print("Número más grande:", max(numerosRandom))
+print("Número más chico:", min(numerosRandom))
+
+numerosRandom.sort()
+print("Lista ordenada:", numerosRandom)
+
+
+#ejercicio 5
+
+listaNumeros = [1,2,3,4,5,6,7,8,9,10]
+
+soloPares = []
+soloImpares = []
+
+for numero in listaNumeros:
+    if numero % 2 == 0:
+        soloPares = soloPares + [numero]
+    else:
+        soloImpares = soloImpares + [numero]
+
+print("Números pares:", soloPares)
+print("Números impares:", soloImpares)
+
+
+#ejercicio6
+
+usuariosRegistrados = ["Alfonso","Cuevas","Pato"]
+
+nuevoUsuario = input("Ingresa un nombre de usuario: ")
+
+while nuevoUsuario in usuariosRegistrados:
+    print("Ese usuario ya existe, intenta con otro.")
+    nuevoUsuario = input("Ingresa un nombre de usuario: ")
+
+usuariosRegistrados = usuariosRegistrados + [nuevoUsuario]
+print("Usuarios registrados:", usuariosRegistrados)
 
